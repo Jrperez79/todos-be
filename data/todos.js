015d4473f3078b5f6a2d3bacf49817783eb1db37
Jrperez79 <joelrperez@gmail.com>
2020-08-18 14:05:09 -0700

    updated data schema in todos.js

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'wash the dishes',
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: 'sweep the floor',
+    completed: false,
+  },
+  {
+    id: 1,
+    todo: 'take out the trash',
+    completed: false,
+  }
+];
